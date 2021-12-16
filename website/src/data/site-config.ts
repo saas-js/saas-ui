ae6e324e518ddfef0add47b29787aeeb45ebd64e
Eelco Wiersma <contact@pagebakers.nl>
2021-12-16 19:19:41 +0000

    Update website config

--- website/src/data/site-config.ts
+++ website/src/data/site-config.ts
@@ -1,64 +1,53 @@
 const baseUrl = 'https://github.com/chakra-ui/chakra-ui'
 
 const siteConfig = {
-  copyright: `Copyright © ${new Date().getFullYear()} Segun Adebayo. All Rights Reserved.`,
-  algolia: {
-    apiKey: 'df1dcc41f7b8e5d68e73dd56d1e19701',
-    indexName: 'chakra-ui',
-    inputSelector: '#algolia-search',
-  },
+  copyright: `Copyright © ${new Date().getFullYear()} Appulse Software B.V. All Rights Reserved.`,
   author: {
-    name: 'Segun Adebayo',
-    github: 'https://github.com/segunadebayo',
-    twitter: 'https://twitter.com/thesegunadebayo',
-    linkedin: 'https://linkedin.com/in/thesegunadebayo',
-    email: 'sage@adebayosegun.com',
+    name: 'Eelco Wiersma',
+    github: 'https://github.com/Pagebakers',
+    twitter: 'https://twitter.com/Pagebakers',
+    linkedin: 'https://linkedin.com/in/eelcowiersma',
+    email: 'hello@saas-ui.dev',
   },
   repo: {
     url: baseUrl,
     editUrl: `${baseUrl}/edit/main/website/pages`,
     blobUrl: `${baseUrl}/blob/main`,
   },
-  openCollective: {
-    url: 'https://opencollective.com/chakra-ui',
-  },
-  discord: {
-    url: 'https://discord.gg/chakra-ui',
-  },
-  youtube: 'https://www.youtube.com/channel/UC4TmDovH46TB4S0SM0Y4CIg',
+  // discord: {
+  //   url: 'https://discord.gg/chakra-ui',
+  // },
+  // youtube: 'https://www.youtube.com/channel/UC4TmDovH46TB4S0SM0Y4CIg',
   seo: {
-    title: 'Chakra UI',
-    titleTemplate: '%s - Chakra UI',
-    description:
-      'Simple, Modular and Accessible UI Components for your React Applications.',
-    siteUrl: 'https://chakra-ui.com',
+    title: 'Saas UI',
+    titleTemplate: '%s - Saas UI',
+    description: 'Advanced UI components for SaaS products.',
+    siteUrl: 'https://saas-ui.dev',
     twitter: {
-      handle: '@chakra-ui',
-      site: '@chakra-ui',
+      handle: '@SaasUI_Dev-uidev',
+      site: '@SaasUI_Dev',
       cardType: 'summary_large_image',
     },
     openGraph: {
       type: 'website',
       locale: 'en_US',
-      url: 'https://chakra-ui.com',
-      title: 'Chakra UI',
-      description:
-        'Simple, Modular and Accessible UI Components for your React Applications.',
-      site_name:
-        'Chakra UI: Simple, Modular and Accessible UI Components for your React Applications.',
+      url: 'https://saas-ui.dev',
+      title: 'Saas UI',
+      description: 'Advanced UI components for SaaS products.',
+      site_name: 'Saas UI: Advanced UI components for SaaS products.',
       images: [
-        {
-          url: 'https://chakra-ui.com/og-image.png',
-          width: 1240,
-          height: 480,
-          alt: 'Chakra UI: Simple, Modular and Accessible UI Components for your React Applications.',
-        },
-        {
-          url: 'https://chakra-ui.com/twitter-og-image.png',
-          width: 1012,
-          height: 506,
-          alt: 'Chakra UI: Simple, Modular and Accessible UI Components for your React Applications.',
-        },
+        // {
+        //   url: 'https://chakra-ui.com/og-image.png',
+        //   width: 1240,
+        //   height: 480,
+        //   alt: 'Chakra UI: Simple, Modular and Accessible UI Components for your React Applications.',
+        // },
+        // {
+        //   url: 'https://chakra-ui.com/twitter-og-image.png',
+        //   width: 1012,
+        //   height: 506,
+        //   alt: 'Chakra UI: Simple, Modular and Accessible UI Components for your React Applications.',
+        // },
       ],
     },
   },

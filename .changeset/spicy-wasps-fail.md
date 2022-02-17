7b87e43331d57834d67fa3f1f76572ab8e6d00be
Eelco Wiersma <contact@pagebakers.nl>
2022-02-17 13:20:06 +0000

    Fix overflow menu icon color

---
+++ .changeset/spicy-wasps-fail.md
@@ -0,0 +1,5 @@
+---
+'@saas-ui/menu': patch
+---
+
+Use currentColor on stroke
--- packages/saas-ui-menu/src/icons.tsx
+++ packages/saas-ui-menu/src/icons.tsx
@@ -3,7 +3,7 @@ import { Icon, IconProps } from '@chakra-ui/icons'
 
 export const MoreIcon = (props: IconProps) => {
   return (
-    <Icon viewBox="0 0 24 24" strokeWidth="2" {...props}>
+    <Icon viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" {...props}>
       <circle cx="12" cy="12" r="1"></circle>
       <circle cx="12" cy="5" r="1"></circle>
       <circle cx="12" cy="19" r="1"></circle>

60130943d54fc359a1c2a3bc54de903928adbeec
Eelco Wiersma <contact@pagebakers.nl>
2022-03-03 15:44:02 +0000

    Fix loader test

--- packages/saas-ui-layout/tests/loader.test.tsx
+++ packages/saas-ui-layout/tests/loader.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
 import { render, testStories } from '@saas-ui/test-utils'
-import * as stories from '../stories/loading.stories'
+import * as stories from '../stories/loader.stories'
 
 testStories<typeof stories>(stories)

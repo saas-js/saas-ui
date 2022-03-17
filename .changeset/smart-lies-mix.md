29a7479bc36b6f7f94fccacb7010f71c9f4b82da
Eelco Wiersma <contact@pagebakers.nl>
2022-03-17 18:02:16 +0000

    Improved MenuDialog api

---
+++ .changeset/smart-lies-mix.md
@@ -0,0 +1,5 @@
+---
+'@saas-ui/modals': minor
+---
+
+Improved the MenuDialog api to be more consistant with Menu
--- packages/saas-ui-modals/src/menu.tsx
+++ packages/saas-ui-modals/src/menu.tsx
@@ -2,20 +2,17 @@ import * as React from 'react'
 
 import {
   ModalFooter,
-  MenuDescendantsProvider,
-  MenuProvider,
   MenuListProps,
-  useMenu,
   useMenuList,
   chakra,
   StylesProvider,
-  useMultiStyleConfig,
-  useTheme,
   useStyles,
   forwardRef,
+  useMenuContext,
+  Menu,
 } from '@chakra-ui/react'
 
-import { BaseModal, BaseModalProps } from './modal'
+import { BaseModal, BaseModalProps } from '@saas-ui/modals'
 
 export interface MenuDialogProps extends BaseModalProps {
   /**
@@ -25,63 +22,59 @@ export interface MenuDialogProps extends BaseModalProps {
 }
 
 export const MenuDialog: React.FC<MenuDialogProps> = (props) => {
-  const { children, footer, isOpen, onClose, ...rest } = props
-
-  const styles = useMultiStyleConfig('Menu', props)
-  const { direction } = useTheme()
-  const { descendants, ...ctx } = useMenu({
-    onClose,
-    autoSelect: true,
-    defaultIsOpen: true,
-    closeOnBlur: false,
-    direction,
-  })
-  const context = React.useMemo(() => ctx, [ctx])
-
-  React.useEffect(() => {
-    ctx.openAndFocusFirstItem()
-  }, [props.isOpen])
-
-  return (
-    <BaseModal
-      isOpen={isOpen}
-      onClose={onClose}
-      initialFocusRef={ctx.menuRef}
-      {...rest}
-    >
-      <MenuDescendantsProvider value={descendants}>
-        <MenuProvider value={context}>
-          <StylesProvider value={styles}>{children}</StylesProvider>
-        </MenuProvider>
-      </MenuDescendantsProvider>
-
-      {footer && <ModalFooter>{footer}</ModalFooter>}
-    </BaseModal>
-  )
+  return <Menu variant="dialog" {...props} />
 }
 
-MenuDialog.defaultProps = {
-  variant: 'dialog',
-}
+export interface MenuDialogListProps
+  extends Omit<
+      BaseModalProps,
+      'isOpen' | 'onClose' | 'children' | 'scrollBehavior'
+    >,
+    Omit<MenuListProps, 'title'> {}
 
-export const MenuDialogList = forwardRef<MenuListProps, 'div'>((props, ref) => {
-  const { rootProps, ...rest } = props
+export const MenuDialogList = forwardRef<MenuDialogListProps, 'div'>(
+  (props, ref) => {
+    const {
+      rootProps,
+      title,
+      footer,
+      initialFocusRef,
+      hideCloseButton,
+      motionPreset,
+      ...rest
+    } = props
 
-  const ownProps = useMenuList(rest, ref) as any
+    const { isOpen, onClose, menuRef } = useMenuContext()
 
-  const styles = useStyles()
+    const ownProps = useMenuList(rest, ref)
 
-  return (
-    <chakra.div
-      {...ownProps}
-      __css={{
-        outline: 0,
-        maxHeight: '80vh', // can override this in theme
-        overflowY: 'auto', // can override this in theme
-        ...styles.list,
-        boxShadow: 'none',
-        border: 0,
-      }}
-    />
-  )
-})
+    const styles = useStyles()
+
+    return (
+      <BaseModal
+        isOpen={isOpen}
+        onClose={onClose}
+        initialFocusRef={initialFocusRef || menuRef}
+        title={title}
+        hideCloseButton={hideCloseButton}
+        motionPreset={motionPreset}
+      >
+        {/* We forward the styles again, otherwise the modal styles will be picked up */}
+        <StylesProvider value={styles}>
+          <chakra.div
+            {...ownProps}
+            __css={{
+              outline: 0,
+              maxHeight: '80vh', // can override this in theme
+              overflowY: 'auto', // can override this in theme
+              ...styles.list,
+              boxShadow: 'none',
+              border: 0,
+            }}
+          />
+        </StylesProvider>
+        {footer && <ModalFooter>{footer}</ModalFooter>}
+      </BaseModal>
+    )
+  }
+)

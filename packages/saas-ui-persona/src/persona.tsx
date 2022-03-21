ab3c077ed25cf751a5cf8ef7a1a53f11b69c8fe7
Eelco Wiersma <contact@pagebakers.nl>
2022-03-21 10:01:01 +0000

    PersonaContainer now accepts size and box props (#26) and added BEM classnames.

--- packages/saas-ui-persona/src/persona.tsx
+++ packages/saas-ui-persona/src/persona.tsx
@@ -19,7 +19,10 @@ import {
   useColorModeValue,
   useTheme,
   useMultiStyleConfig,
+  omitThemingProps,
 } from '@chakra-ui/system'
+
+import { cx } from '@chakra-ui/utils'
 
 export interface PresenceOptions {
   [presence: string]: string
@@ -52,204 +55,6 @@ export const defaultPresenceTokens = {
   dnd: 'red.500',
   away: 'gray.400',
 }
-
-export interface PersonaContainerProps
-  extends HTMLChakraProps<'div'>,
-    Omit<ThemingProps<'Persona'>, 'size'> {}
-
-export const PersonaContainer = forwardRef<PersonaContainerProps, 'div'>(
-  (props, ref) => {
-    const { children } = props
-    const styles = useMultiStyleConfig('Persona', props)
-
-    const baseStyle: SystemStyleObject = {
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-    }
-
-    const containerStyles = {
-      ...baseStyle,
-      ...styles.container,
-    }
-
-    return (
-      <StylesProvider value={styles}>
-        <chakra.div ref={ref} __css={containerStyles}>
-          {children}
-        </chakra.div>
-      </StylesProvider>
-    )
-  }
-)
-
-interface PresenceAvatarOptions {
-  /**
-   * The name of the person in the avatar.
-   *
-   * - if `src` has loaded, the name will be used as the `alt` attribute of the `img`
-   * - If `src` is not loaded, the name will be used to create the initials
-   */
-  name?: string
-  /**
-   * The presence status of the person
-   *
-   * If set will add an AvatarBadge with color configured in `Presence`
-   * Default presence options:
-   * - online
-   * - offline
-   * - busy
-   * - dnd
-   * - away
-   */
-  presence?: string
-  /**
-   * The icon shown in the AvatarBadge
-   */
-  presenceIcon?: React.ReactNode
-  /**
-   * The badge size. Defaults to 1em. Use em value to keep the size relative to the avatar.
-   */
-  badgeSize?: SystemProps['boxSize']
-  /**
-   * Indicates that a person is out of office. Changes the presence badge style.
-   */
-  isOutOfOffice?: boolean
-}
-
-interface PresenceAvatarProps extends PresenceAvatarOptions, AvatarProps {}
-
-export const PersonaAvatar = forwardRef<PresenceAvatarProps, 'span'>(
-  (props, ref) => {
-    const {
-      name,
-      presence,
-      presenceIcon,
-      isOutOfOffice,
-      badgeSize = '1em',
-      size,
-      getInitials,
-      icon,
-      iconLabel,
-      ignoreFallback,
-      loading,
-      onError,
-      src,
-      srcSet,
-    } = props
-    const badgeProps: AvatarBadgeProps = {}
-    let badge
-
-    const theme = useTheme()
-
-    const colors = theme.colors?.presence || defaultPresenceTokens
-    const semantic = theme.semanticTokens?.colors?.['presence.online']
-
-    if (presence) {
-      const color = semantic ? `presence.${presence}` : colors[presence]
-      if (isOutOfOffice) {
-        badgeProps.sx = {
-          _before: {
-            content: '""',
-            width: '100%',
-            height: '100%',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            border: '0.2em solid',
-            borderColor: color,
-            borderRadius: '50%',
-          },
-        }
-        badgeProps.borderWidth = '0.15em'
-        badgeProps.bg = useColorModeValue('white', 'gray.800')
-      } else {
-        badgeProps.bg = color
-      }
-
-      badge = (
-        <AvatarBadge boxSize={badgeSize} {...badgeProps}>
-          {presenceIcon}
-        </AvatarBadge>
-      )
-    }
-
-    return (
-      <Avatar
-        ref={ref}
-        name={name}
-        size={size}
-        getInitials={getInitials}
-        icon={icon}
-        iconLabel={iconLabel}
-        ignoreFallback={ignoreFallback}
-        loading={loading}
-        onError={onError}
-        src={src}
-        srcSet={srcSet}
-      >
-        {badge}
-      </Avatar>
-    )
-  }
-)
-
-export const PersonaDetails = forwardRef<PersonaProps, 'div'>((props, ref) => {
-  const { children } = props
-  const styles = useStyles()
-
-  const baseStyle: SystemStyleObject = {
-    display: 'flex',
-    flexDirection: 'column',
-  }
-
-  const detailsStyles = {
-    ...baseStyle,
-    ...styles.details,
-  }
-
-  return (
-    <chakra.div ref={ref} __css={detailsStyles}>
-      {children}
-    </chakra.div>
-  )
-})
-
-export const PersonaLabel = forwardRef<HTMLChakraProps<'span'>, 'span'>(
-  (props, ref) => {
-    const styles = useStyles()
-    return <chakra.span ref={ref} isTruncated {...props} __css={styles.label} />
-  }
-)
-
-export const PersonaSecondaryLabel = forwardRef<
-  HTMLChakraProps<'span'>,
-  'span'
->((props, ref) => {
-  const styles = useStyles()
-  return (
-    <chakra.span
-      ref={ref}
-      isTruncated
-      {...props}
-      __css={styles.secondaryLabel}
-    />
-  )
-})
-
-export const PersonaTertiaryLabel = forwardRef<HTMLChakraProps<'span'>, 'span'>(
-  (props, ref) => {
-    const styles = useStyles()
-    return (
-      <chakra.span
-        ref={ref}
-        isTruncated
-        {...props}
-        __css={styles.tertiaryLabel}
-      />
-    )
-  }
-)
 
 interface PersonaOptions {
   /**
@@ -363,3 +168,226 @@ export const Persona: React.FC<PersonaProps> = (props) => {
     </PersonaContainer>
   )
 }
+
+export interface PersonaContainerProps
+  extends HTMLChakraProps<'div'>,
+    Omit<ThemingProps<'Persona'>, 'size'>,
+    Pick<PersonaOptions, 'size'> {}
+
+export const PersonaContainer = forwardRef<PersonaContainerProps, 'div'>(
+  (props, ref) => {
+    const { children, ...rest } = props
+    const styles = useMultiStyleConfig('Persona', props)
+
+    const containerProps = omitThemingProps(rest)
+
+    const baseStyle: SystemStyleObject = {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    }
+
+    const containerStyles = {
+      ...baseStyle,
+      ...styles.container,
+    }
+
+    return (
+      <StylesProvider value={styles}>
+        <chakra.div
+          ref={ref}
+          __css={containerStyles}
+          {...containerProps}
+          className={cx('saas-persona', props.className)}
+        >
+          {children}
+        </chakra.div>
+      </StylesProvider>
+    )
+  }
+)
+
+interface PresenceAvatarOptions {
+  /**
+   * The name of the person in the avatar.
+   *
+   * - if `src` has loaded, the name will be used as the `alt` attribute of the `img`
+   * - If `src` is not loaded, the name will be used to create the initials
+   */
+  name?: string
+  /**
+   * The presence status of the person
+   *
+   * If set will add an AvatarBadge with color configured in `Presence`
+   * Default presence options:
+   * - online
+   * - offline
+   * - busy
+   * - dnd
+   * - away
+   */
+  presence?: string
+  /**
+   * The icon shown in the AvatarBadge
+   */
+  presenceIcon?: React.ReactNode
+  /**
+   * The badge size. Defaults to 1em. Use em value to keep the size relative to the avatar.
+   */
+  badgeSize?: SystemProps['boxSize']
+  /**
+   * Indicates that a person is out of office. Changes the presence badge style.
+   */
+  isOutOfOffice?: boolean
+}
+
+interface PresenceAvatarProps extends PresenceAvatarOptions, AvatarProps {}
+
+export const PersonaAvatar = forwardRef<PresenceAvatarProps, 'span'>(
+  (props, ref) => {
+    const {
+      name,
+      presence,
+      presenceIcon,
+      isOutOfOffice,
+      badgeSize = '1em',
+      size,
+      getInitials,
+      icon,
+      iconLabel,
+      ignoreFallback,
+      loading,
+      onError,
+      src,
+      srcSet,
+      ...rest
+    } = props
+    const badgeProps: AvatarBadgeProps = {}
+    let badge
+
+    const theme = useTheme()
+
+    const colors = theme.colors?.presence || defaultPresenceTokens
+    const semantic = theme.semanticTokens?.colors?.['presence.online']
+
+    if (presence) {
+      const color = semantic ? `presence.${presence}` : colors[presence]
+      if (isOutOfOffice) {
+        badgeProps.sx = {
+          _before: {
+            content: '""',
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            border: '0.2em solid',
+            borderColor: color,
+            borderRadius: '50%',
+          },
+        }
+        badgeProps.borderWidth = '0.15em'
+        badgeProps.bg = useColorModeValue('white', 'gray.800')
+      } else {
+        badgeProps.bg = color
+      }
+
+      badge = (
+        <AvatarBadge boxSize={badgeSize} {...badgeProps}>
+          {presenceIcon}
+        </AvatarBadge>
+      )
+    }
+
+    return (
+      <Avatar
+        ref={ref}
+        name={name}
+        size={size}
+        getInitials={getInitials}
+        icon={icon}
+        iconLabel={iconLabel}
+        ignoreFallback={ignoreFallback}
+        loading={loading}
+        onError={onError}
+        src={src}
+        srcSet={srcSet}
+        {...rest}
+      >
+        {badge}
+      </Avatar>
+    )
+  }
+)
+
+export const PersonaDetails = forwardRef<PersonaProps, 'div'>((props, ref) => {
+  const { children, className, ...rest } = props
+  const styles = useStyles()
+
+  const baseStyle: SystemStyleObject = {
+    display: 'flex',
+    flexDirection: 'column',
+  }
+
+  const detailsStyles = {
+    ...baseStyle,
+    ...styles.details,
+  }
+
+  return (
+    <chakra.div
+      ref={ref}
+      {...rest}
+      __css={detailsStyles}
+      className={cx('saas-persona__details', className)}
+    >
+      {children}
+    </chakra.div>
+  )
+})
+
+export const PersonaLabel = forwardRef<HTMLChakraProps<'span'>, 'span'>(
+  (props, ref) => {
+    const styles = useStyles()
+    return (
+      <chakra.span
+        ref={ref}
+        isTruncated
+        {...props}
+        __css={styles.label}
+        className={cx('saas-persona__label', props.className)}
+      />
+    )
+  }
+)
+
+export const PersonaSecondaryLabel = forwardRef<
+  HTMLChakraProps<'span'>,
+  'span'
+>((props, ref) => {
+  const styles = useStyles()
+  return (
+    <chakra.span
+      ref={ref}
+      isTruncated
+      {...props}
+      __css={styles.secondaryLabel}
+      className={cx('saas-persona__secondary-label', props.className)}
+    />
+  )
+})
+
+export const PersonaTertiaryLabel = forwardRef<HTMLChakraProps<'span'>, 'span'>(
+  (props, ref) => {
+    const styles = useStyles()
+    return (
+      <chakra.span
+        ref={ref}
+        isTruncated
+        {...props}
+        __css={styles.tertiaryLabel}
+        className={cx('saas-persona__tertiary-label', props.className)}
+      />
+    )
+  }
+)

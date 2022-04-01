767077f54be68f2c602914669863f03d9dc9ebe4
Eelco Wiersma <contact@pagebakers.nl>
2022-04-01 10:22:40 +0200

    Make sure the yup resolver packages gets transpiled by Nextjs

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -60,7 +60,7 @@ let config = {
 
     config.plugins = config.plugins.concat([
       new webpack.NormalModuleReplacementPlugin(
-        /\@saas-ui\/(?!props-docs)([a-z0-9-]+)$/,
+        /\@saas-ui\/(?!props-docs)([a-z0-9-\/]+)$/,
         (resource) => {
           resource.request = resource.request + '/src'
         }

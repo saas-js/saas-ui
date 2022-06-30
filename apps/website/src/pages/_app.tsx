237dc1e93118e222c47369e32fa7382cc463372c
Eelco Wiersma <contact@pagebakers.nl>
2022-06-30 09:44:58 +0200

    Fixed woopra loading

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -24,7 +24,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
             footer={pageProps.footer !== false ? <Footer /> : null}
           >
             <Script
-              id="woopra"
+              id="woopra-js"
               strategy="afterInteractive"
               dangerouslySetInnerHTML={{
                 __html: `!function(){var t,o,c,e=window,n=document,r=arguments,a="script",i=["call","cancelAction","config","identify","push","track","trackClick","trackForm","update","visit"],s=function(){var t,o=this,c=function(t){o[t]=function(){return o._e.push([t].concat(Array.prototype.slice.call(arguments,0))),o}};for(o._e=[],t=0;t<i.length;t++)c(i[t])};for(e.__woo=e.__woo||{},t=0;t<r.length;t++)e.__woo[r[t]]=e[r[t]]=e[r[t]]||new s;(o=n.createElement(a)).async=1,o.src="https://static.woopra.com/js/w.js",(c=n.getElementsByTagName(a)[0]).parentNode.insertBefore(o,c)}("woopra");

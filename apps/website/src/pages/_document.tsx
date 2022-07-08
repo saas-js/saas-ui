f051dd51435bfbd8c53b4ca0d2549a05ba9ae666
Eelco Wiersma <contact@pagebakers.nl>
2022-07-08 14:01:13 +0200

    Updated favicons

--- apps/website/src/pages/_document.tsx
+++ apps/website/src/pages/_document.tsx
@@ -14,10 +14,24 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content="#FFFFFF" />
-
-          <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/manifest.json" />
         </Head>
         <body>
           <Main />

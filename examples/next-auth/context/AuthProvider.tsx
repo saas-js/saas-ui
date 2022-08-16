35796a6069b70a2bd8274a0c66d3dbee0300ecfa
Eelco Wiersma <contact@pagebakers.nl>
2022-08-16 20:41:52 +0200

    docs: added NextAuth example

---
+++ examples/next-auth/context/AuthProvider.tsx
@@ -0,0 +1,64 @@
+import { AuthProvider, AuthProviderProps, User } from '@saas-ui/react'
+import { SessionProvider, signIn, signOut, useSession } from 'next-auth/react'
+import { Session } from 'next-auth'
+import React from 'react'
+
+const createAuthService = (session: Session | null): AuthProviderProps => {
+  return {
+    onLogin: async ({ provider, email }, options) => {
+      const type = provider ?? 'email'
+
+      let params: Record<string, any> = {
+        callbackUrl: options?.redirectTo,
+      }
+
+      if (email) {
+        params = {
+          email,
+          redirect: false, // do not redirect to NextAuth login page
+          ...params,
+        }
+      }
+
+      const result = await signIn(type, params)
+
+      if (result?.ok) {
+        //
+      }
+
+      return undefined
+    },
+    onLogout: () => signOut(),
+    onLoadUser: async () => (session?.user as User) || null,
+    onGetToken: async () => {
+      return session?.user?.email
+    },
+  }
+}
+
+export interface NextAuthProviderProps {
+  /**
+   * Get this with getInitialProps or getServerSideProps
+   */
+  session: Session
+  children: React.ReactNode
+}
+
+export const NextAuthProvider: React.FC<NextAuthProviderProps> = (props) => {
+  const { children, session } = props
+  return (
+    <SessionProvider session={session}>
+      <Provider>{children}</Provider>
+    </SessionProvider>
+  )
+}
+
+/**
+ * Wrap the AuthProvider here so we can get access to the NextAuth session
+ */
+const Provider: React.FC<React.PropsWithChildren> = (props) => {
+  const { data } = useSession()
+  return (
+    <AuthProvider {...createAuthService(data)}>{props.children}</AuthProvider>
+  )
+}
---
+++ examples/next-auth/pages/index.tsx
@@ -0,0 +1,28 @@
+import { Box, Button } from '@chakra-ui/react'
+import { Loader, useAuth } from '@saas-ui/react'
+import NextLink from 'next/link'
+
+const IndexPage = () => {
+  const { isLoggingIn, user, logOut } = useAuth()
+
+  if (isLoggingIn) {
+    return <Loader variant="fullscreen" />
+  } else if (!user) {
+    return (
+      <Box>
+        <NextLink href="/login">
+          <Button>Log in</Button>
+        </NextLink>
+      </Box>
+    )
+  }
+
+  return (
+    <Box>
+      Logged in as: {user.email}.{' '}
+      <Button onClick={() => logOut()}>Log out</Button>
+    </Box>
+  )
+}
+
+export default IndexPage

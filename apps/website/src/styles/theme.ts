8e868b3a7f8fb68c4bf07d10e59576eedbb22f9e
Eelco Wiersma <contact@pagebakers.nl>
2022-09-17 16:13:01 +0200

    fix: update types

--- apps/website/src/styles/theme.ts
+++ apps/website/src/styles/theme.ts
@@ -1,6 +1,6 @@
 import { extendTheme } from '@chakra-ui/react'
 
-import { theme as baseTheme } from '@saas-ui/pro/theme'
+import { theme as baseTheme } from '@saas-ui/pro'
 
 import '@fontsource/inter/variable.css'
 

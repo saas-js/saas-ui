6427428b3050d1b643266fae6028510e14856d76
Pagebakers <eelco@appulse.nl>
2023-04-16 11:04:30 +0200

    chore: add lostpixel workflow

---
+++ lostpixel.config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  storybookShots: {
+    storybookUrl: './packages/storybook/storybook-static',
+  },
+  lostPixelProjectId: 'clg3iy33z0b91ie0e4hqa0ws7',
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+}

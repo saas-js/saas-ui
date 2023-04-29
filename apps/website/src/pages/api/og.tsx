99e784528a08fbd4faa19fdeb1f50332b67117a2
Pagebakers <eelco@appulse.nl>
2023-04-29 14:57:35 +0200

    chore: update og

--- apps/website/src/pages/api/og.tsx
+++ apps/website/src/pages/api/og.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
 
 export const config = {
-  runtime: 'experimental-edge',
+  runtime: 'edge',
 }
 
 const font = fetch(

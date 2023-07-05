9b609119baf581b7a47275c24de0f7657257f846
Pagebakers <eelco@appulse.nl>
2023-07-05 16:40:45 +0200

    fix: pass link props correctly

---
+++ .changeset/silly-walls-lie.md
@@ -0,0 +1,6 @@
+---
+'@saas-ui/core': patch
+'@saas-ui/react': patch
+---
+
+Fix issue where Link would not pass props correctly.
--- packages/saas-ui-core/src/provider/use-link.tsx
+++ packages/saas-ui-core/src/provider/use-link.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { chakra } from '@chakra-ui/react'
 import { useSaas } from './saas-provider'
 
-const Link: React.FC<any> = ({ props }) => <chakra.a {...props} />
+const Link: React.FC<any> = (props) => <chakra.a {...props} />
 
 export function useLink(): React.ElementType<any> {
   const context = useSaas()

848a3e2a25c2b61b0778e9e744cd158613540129
Pagebakers <eelco@appulse.nl>
2023-07-14 15:34:30 +0200

    chore: update next auth to saas ui 2.0

--- examples/next-auth/pages/index.tsx
+++ examples/next-auth/pages/index.tsx
@@ -1,12 +1,17 @@
 import { Box, Button } from '@chakra-ui/react'
-import { Loader, useAuth } from '@saas-ui/react'
+import { LoadingOverlay, LoadingSpinner } from '@saas-ui/react'
+import { useAuth } from '@saas-ui/auth'
 import NextLink from 'next/link'
 
 const IndexPage = () => {
   const { isLoggingIn, user, logOut } = useAuth()
 
   if (isLoggingIn) {
-    return <Loader variant="fullscreen" />
+    return (
+      <LoadingOverlay variant="fullscreen">
+        <LoadingSpinner />
+      </LoadingOverlay>
+    )
   } else if (!user) {
     return (
       <Box>

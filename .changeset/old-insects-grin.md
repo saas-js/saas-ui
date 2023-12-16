c7aff7d522dc991e9ec4f1b8ec525cd3297d4be1
Pagebakers <eelco@appulse.nl>
2023-12-16 10:47:10 +0100

    fix: fixed issue where SidebarToggleButton style props would not have any effect

---
+++ .changeset/old-insects-grin.md
@@ -0,0 +1,6 @@
+---
+'@saas-ui/core': patch
+'@saas-ui/react': patch
+---
+
+Fixed issue where SidebarToggleButton style props would not have any effect
--- packages/saas-ui-core/src/sidebar/sidebar.tsx
+++ packages/saas-ui-core/src/sidebar/sidebar.tsx
@@ -174,40 +174,56 @@ Sidebar.id = 'Sidebar'
 export const SidebarToggleButton: React.FC<SidebarToggleButtonProps> = (
   props
 ) => {
-  const { sx, pos, position, ...rest } = props
+  const { sx, pos, position, variant = 'ghost', icon, isRound, ...rest } = props
   const { isOpen, isMobile, getButtonProps } = useSidebarToggleButton()
   const styles = useStyleConfig('SuiSidebarToggleButton', props)
+  const btnStyles = useStyleConfig('Button', {
+    ...props,
+    variant,
+  })
 
   const p = pos ?? position ?? sx?.pos ?? sx?.position
 
   const buttonStyles: SystemStyleObject = {
+    display: 'inline-flex',
+    appearance: 'none',
+    alignItems: 'center',
+    justifyContent: 'center',
+    userSelect: 'none',
+    whiteSpace: 'nowrap',
+    verticalAlign: 'middle',
+    outline: 'none',
+    ...btnStyles,
     ...(isMobile
       ? !p
         ? { position: 'fixed', top: 3, left: 4, zIndex: 'modal' }
         : {}
       : { display: 'none' }),
     ...styles,
-    ...sx,
   }
 
-  const icon = props.icon ? (
-    runIfFn(props.icon, {
+  const _icon = icon ? (
+    runIfFn(icon, {
       isOpen,
     })
   ) : (
-    <HamburgerIcon />
+    <HamburgerIcon aria-hidden="true" focusable="false" />
   )
 
   return (
-    <IconButton
-      variant="ghost"
-      sx={buttonStyles}
+    <chakra.button
+      __css={buttonStyles}
+      padding="0"
+      borderRadius={isRound ? 'full' : undefined}
       aria-label={isOpen ? 'Close sidebar' : 'Open sidebar'}
       {...rest}
       {...getButtonProps(props)}
+      sx={sx}
       icon={icon as any}
       className={cx('sui-sidebar__toggle-button', props.className)}
-    />
+    >
+      {_icon}
+    </chakra.button>
   )
 }
 

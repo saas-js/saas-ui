ce3bc9e08744e5baf23bee6287a3062b3fc9eec8
Pagebakers <eelco@appulse.nl>
2024-01-09 09:58:32 +0000

    chore: add vite tsr plugin

--- examples/vite-tsr-spa/src/routeTree.gen.ts
+++ examples/vite-tsr-spa/src/routeTree.gen.ts
@@ -1,70 +1,68 @@
-import { Route as rootRoute } from "./routes/__root"
-import { Route as AuthImport } from "./routes/_auth"
-import { Route as AppImport } from "./routes/_app"
-import { Route as IndexImport } from "./routes/index"
-import { Route as AuthLoginImport } from "./routes/_auth/login"
-import { Route as AppGettingStartedImport } from "./routes/_app/getting-started"
-import { Route as AppWorkspaceIndexImport } from "./routes/_app/$workspace/index"
+import { Route as rootRoute } from './routes/__root'
+import { Route as AuthImport } from './routes/_auth'
+import { Route as AppImport } from './routes/_app'
+import { Route as IndexImport } from './routes/index'
+import { Route as AuthLoginImport } from './routes/_auth/login'
+import { Route as AppGettingStartedImport } from './routes/_app/getting-started'
+import { Route as AppWorkspaceIndexImport } from './routes/_app/$workspace/index'
 
 const AuthRoute = AuthImport.update({
-  id: "/_auth",
+  id: '/_auth',
   getParentRoute: () => rootRoute,
 } as any)
 
 const AppRoute = AppImport.update({
-  id: "/_app",
+  id: '/_app',
   getParentRoute: () => rootRoute,
 } as any)
 
 const IndexRoute = IndexImport.update({
-  path: "/",
+  path: '/',
   getParentRoute: () => rootRoute,
 } as any)
 
 const AuthLoginRoute = AuthLoginImport.update({
-  path: "/login",
+  path: '/login',
   getParentRoute: () => AuthRoute,
 } as any)
 
 const AppGettingStartedRoute = AppGettingStartedImport.update({
-  path: "/getting-started",
+  path: '/getting-started',
   getParentRoute: () => AppRoute,
 } as any)
 
 const AppWorkspaceIndexRoute = AppWorkspaceIndexImport.update({
-  path: "/$workspace/",
+  path: '/$workspace/',
   getParentRoute: () => AppRoute,
 } as any)
-
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/": {
+    '/': {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
-    "/_app": {
+    '/_app': {
       preLoaderRoute: typeof AppImport
       parentRoute: typeof rootRoute
     }
-    "/_auth": {
+    '/_auth': {
       preLoaderRoute: typeof AuthImport
       parentRoute: typeof rootRoute
     }
-    "/_app/getting-started": {
+    '/_app/getting-started': {
       preLoaderRoute: typeof AppGettingStartedImport
-      parentRoute: typeof AppRoute
+      parentRoute: typeof AppImport
     }
-    "/_auth/login": {
+    '/_auth/login': {
       preLoaderRoute: typeof AuthLoginImport
-      parentRoute: typeof AuthRoute
+      parentRoute: typeof AuthImport
     }
-    "/_app/$workspace/": {
+    '/_app/$workspace/': {
       preLoaderRoute: typeof AppWorkspaceIndexImport
-      parentRoute: typeof AppRoute
+      parentRoute: typeof AppImport
     }
   }
 }
-
 export const routeTree = rootRoute.addChildren([
   IndexRoute,
   AppRoute.addChildren([AppGettingStartedRoute, AppWorkspaceIndexRoute]),

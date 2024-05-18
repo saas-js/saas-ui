8ea1f3f813e639871d5e94c0602d9622216b6f43
Pagebakers <eelco@appulse.nl>
2024-05-18 08:58:21 +0200

    chore: update dependencies

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,4 +1,4 @@
-const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require('next-contentlayer2')
 const path = require('node:path')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({

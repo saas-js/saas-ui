1cf0074a5a99ad920249977d0cea62eeddad42ed
Eelco Wiersma <eelco@appulse.nl>
2024-10-14 12:58:39 +0100

    chore:fix links

--- apps/website/src/docs/components/secondary-nav/secondary-nav.tsx
+++ apps/website/src/docs/components/secondary-nav/secondary-nav.tsx
@@ -89,11 +89,11 @@ export const proNav: Links = [
       asPath.startsWith('/docs/pro/packages'),
   },
   {
-    href: '/docs/pro/nextjs-starterkit',
+    href: '/docs/nextjs-starter-kit',
     label: 'Next.js starter kit',
     match: (asPath: string, href: string) =>
-      href.startsWith('/docs/pro/nextjs-starterkit') &&
-      asPath.startsWith('/docs/pro/nextjs-starterkit'),
+      href.startsWith('/docs/nextjs-starter-kit') &&
+      asPath.startsWith('/docs/nextjs-starter-kit'),
   },
 ]
 

c23b419233290b07e99d92fbef768381e850bbfc
Eelco Wiersma <eelco@appulse.nl>
2025-06-01 09:06:01 +0100

    fix: styling

--- apps/website/app/pro/(landing)/pricing/page.tsx
+++ apps/website/app/pro/(landing)/pricing/page.tsx
@@ -1,10 +1,14 @@
+import { ActionArrow } from '@/components/action-arrow'
 import { LinkButton } from '@/components/link-button'
 import { CustomersSection } from '@/components/site/customers.section'
 import {
   Box,
+  ButtonGroup,
   Container,
   Grid,
+  HStack,
   Heading,
+  Icon,
   List,
   Separator,
   Stack,
@@ -13,7 +17,7 @@ import {
 import { Accordion } from '@saas-ui/react'
 import type { Metadata } from 'next'
 import Link from 'next/link'
-import { TbCheck } from 'react-icons/tb'
+import { TbArrowRight, TbCheck } from 'react-icons/tb'
 
 export const metadata: Metadata = {
   title: 'Pricing',
@@ -22,17 +26,19 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <Container maxW="6xl" position="static">
-      <Stack gap="12">
+    <Stack gap="12">
+      <Container maxW="6xl" position="static">
         <Hero />
 
         <Pricing />
 
         <CustomersSection />
+      </Container>
 
-        <FAQ />
-      </Stack>
-    </Container>
+      <Benefits />
+
+      <FAQ />
+    </Stack>
   )
 }
 
@@ -250,53 +256,117 @@ function Pricing() {
 
 function FAQ() {
   return (
-    <Stack maxW="2xl" mx="auto" w="full">
-      <Heading as="h2" fontSize="2xl" fontWeight="medium">
-        FAQ
-      </Heading>
+    <Container maxW="6xl" py="20">
+      <HStack gap="20" alignItems="flex-start">
+        <Stack flex="1" alignItems="flex-start" pt="2">
+          <Heading as="h2" fontSize="2xl" fontWeight="medium">
+            Frequently asked questions
+          </Heading>
 
-      <Stack gap="4" width="full">
-        <Accordion.Root width="full">
-          <Accordion.Item value="1">
-            <Accordion.ItemTrigger>
-              How many products can I use Saas UI Pro for?
-            </Accordion.ItemTrigger>
-            <Accordion.ItemContent>
-              The individual license can be used for unlimited self hosted SaaS
-              products or internal tools and maximum one client project. You can
-              buy as many licenses as you need. The unlimited license does not
-              have any restrictions.
-            </Accordion.ItemContent>
-          </Accordion.Item>
-          <Accordion.Item value="2">
-            <Accordion.ItemTrigger>
-              Does Saas UI include Figma, Sketch or other design files?
-            </Accordion.ItemTrigger>
-            <Accordion.ItemContent>
-              Yes, we have a free Figma community and a Pro libary,{' '}
-              <Link href="/pro/figma">more information.</Link>
-            </Accordion.ItemContent>
-          </Accordion.Item>
-          <Accordion.Item value="3">
-            <Accordion.ItemTrigger>
-              What does the lifetime access mean?
-            </Accordion.ItemTrigger>
-            <Accordion.ItemContent>
-              The Saas UI Pro license is perpetual, you will have access to all
-              assets of the Saas UI Pro library forever.
-            </Accordion.ItemContent>
-          </Accordion.Item>
-          <Accordion.Item value="4">
-            <Accordion.ItemTrigger>Do you offer support?</Accordion.ItemTrigger>
-            <Accordion.ItemContent>
-              Once you sign up you get access to our Discord community, where
-              you can ask questions, report bugs or feature requests and get
-              help from other customers. If you require more specialised support
-              or consultancy contact us at hello@saas-ui.dev
-            </Accordion.ItemContent>
-          </Accordion.Item>
-        </Accordion.Root>
-      </Stack>
-    </Stack>
+          <Text fontWeight="medium" color="fg.subtle">
+            Can't find what you're looking for? Join our Discord community.
+          </Text>
+
+          <ButtonGroup>
+            <LinkButton
+              href="/discord"
+              variant="plain"
+              colorPalette="accent"
+              size="lg"
+            >
+              Join Discord <ActionArrow />
+            </LinkButton>
+          </ButtonGroup>
+        </Stack>
+
+        <Stack gap="4" flex="1">
+          <Accordion.Root width="full" collapsible>
+            <Accordion.Item value="1">
+              <Accordion.ItemTrigger fontSize="md" fontWeight="medium">
+                How many products can I use Saas UI Pro for?
+              </Accordion.ItemTrigger>
+              <Accordion.ItemContent fontSize="md">
+                The individual license can be used for unlimited self hosted
+                SaaS products or internal tools and maximum one client project.
+                You can buy as many licenses as you need. The unlimited license
+                does not have any restrictions.
+              </Accordion.ItemContent>
+            </Accordion.Item>
+            <Accordion.Item value="2">
+              <Accordion.ItemTrigger fontSize="md" fontWeight="medium">
+                Does Saas UI include Figma, Sketch or other design files?
+              </Accordion.ItemTrigger>
+              <Accordion.ItemContent fontSize="md">
+                Yes, we have a free Figma community and a Pro libary,{' '}
+                <Link href="/pro/figma">more information.</Link>
+              </Accordion.ItemContent>
+            </Accordion.Item>
+            <Accordion.Item value="3">
+              <Accordion.ItemTrigger fontSize="md" fontWeight="medium">
+                What does the lifetime access mean?
+              </Accordion.ItemTrigger>
+              <Accordion.ItemContent fontSize="md">
+                The Saas UI Pro license is perpetual, you will have access to
+                all assets of the Saas UI Pro library forever.
+              </Accordion.ItemContent>
+            </Accordion.Item>
+            <Accordion.Item value="4">
+              <Accordion.ItemTrigger fontSize="md" fontWeight="medium">
+                Do you offer support?
+              </Accordion.ItemTrigger>
+              <Accordion.ItemContent fontSize="md">
+                Once you sign up you get access to our Discord community, where
+                you can ask questions, report bugs or feature requests and get
+                help from other customers. If you require more specialised
+                support or consultancy contact us at hello@saas-ui.dev
+              </Accordion.ItemContent>
+            </Accordion.Item>
+          </Accordion.Root>
+        </Stack>
+      </HStack>
+    </Container>
+  )
+}
+
+function Benefits() {
+  return (
+    <Box
+      position="relative"
+      borderTopWidth="1px"
+      borderBottomWidth="1px"
+      borderStyle="dashed"
+    >
+      <Container maxW="6xl" position="static">
+        <HStack>
+          <Stack flex="1">
+            <Stack py="20" gap="8">
+              <Heading
+                as="h3"
+                fontSize="6xl"
+                lineHeight="1"
+                fontWeight="medium"
+              >
+                Build, launch and scale with Saas UI Pro
+              </Heading>
+
+              <Text fontWeight="medium" color="fg.subtle">
+                Saas UI Pro is a comprehensive solution for building, launching
+                and scaling your SaaS. It includes everything you need to build
+                a professional and scalable SaaS.
+              </Text>
+            </Stack>
+          </Stack>
+          <Stack
+            borderLeftWidth="1px"
+            borderStyle="dashed"
+            flex="1"
+            display={{
+              base: 'none',
+              md: 'flex',
+            }}
+          ></Stack>
+        </HStack>
+      </Container>
+    </Box>
   )
 }
